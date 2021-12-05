916277078d21cbc5a3133795ed886a6d924e7366
rachael-lh <rachaellouho@gmail.com>
2021-12-05 00:59:02 +0000

    added docker-compose amd amended app.py to correspond with db

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask (__name__)
 app.secret_key = "Secret Key"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:MYPASSWORD@db-finalproject:3306/MySQLDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:MYPASSWORD@db:3306/MySQLDB'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
