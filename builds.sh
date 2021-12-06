469a6b6eb7d37d19c4ade12b41500de9d13e81e8
rachael-lh <rachaellouho@gmail.com>
2021-12-06 14:34:49 +0000

    amended builds.sh

--- builds.sh
+++ builds.sh
@@ -1,6 +1,9 @@
-chmod +x /usr/local/bin/docker-compose
-usermod -aG docker $(whoami)
-usermod -aG docker jenkins
-chmod 666 /var/run/docker.sock
-docker-compose down --rmi all
-docker-compose build
+#!/bin/bash
+#check the location
+pwd
+
+#install all dependencies
+sudo update -y
+sudo upgrade -y
+sudo apt install -y python3-pip
+sudo pip3 install --upgrade pip
