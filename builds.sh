620ea7f1017a4b5fb96b3fe1cbae1daa9ec8f7b6
rachael-lh <rachaellouho@gmail.com>
2021-12-06 14:32:12 +0000

    amended builds.sh

--- builds.sh
+++ builds.sh
@@ -1,6 +1,6 @@
-sudo chmod +x /usr/local/bin/docker-compose
-sudo usermod -aG docker $(whoami)
-sudo usermod -aG docker jenkins
-sudo chmod 666 /var/run/docker.sock
+chmod +x /usr/local/bin/docker-compose
+usermod -aG docker $(whoami)
+usermod -aG docker jenkins
+chmod 666 /var/run/docker.sock
 docker-compose down --rmi all
 docker-compose build
