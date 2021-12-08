07a2ae091d281ef227fd527089730f0822e15c21
rachael-lh <rachaellouho@gmail.com>
2021-12-08 16:57:10 +0000

    amending test_app.py

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask (__name__)
 app.secret_key = "Secret Key"
