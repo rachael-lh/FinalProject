31d1430c8c4471ca92773de9c13e5ce89ced4668
rachael-lh <rachaellouho@gmail.com>
2021-12-09 23:22:56 +0000

    amended builds.sh

--- builds.sh
+++ builds.sh
@@ -7,9 +7,16 @@ sudo update -y
 sudo upgrade -y
 sudo apt install python3-pip
 sudo pip3 install --upgrade pip
+sudo usermod -aG docker $(whoami)
+
+cd db
+docker build -t db-finalproject .
+
+cd ..
 
 cd flask-app
 pip3 install -r requirements.txt
-sudo usermod -aG docker $(whoami)
+docker build -t flask-app-finalproject . 
+
 cd ..
 docker-compose up -d
