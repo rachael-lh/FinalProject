9fc60872f9088307f22a924c03e8e682f10e2b72
rachael-lh <rachaellouho@gmail.com>
2021-12-09 09:56:07 +0000

    amended sql files

--- db/CreateUserGoalsTable.sql
+++ db/CreateUserGoalsTable.sql
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS User_Goals
 
 LOCK TABLES `User_Goals` WRITE;
 /*!40000 ALTER TABLE `User_Goals` DISABLE KEYS */;
-INSERT INTO `User_Goals` VALUES (1,1,'Finish project work','06/12/2021','10/12/2021'),(2,2,'Revise for AZ900 test','05/12/2021','15/12/2021')
+INSERT INTO `User_Goals` VALUES (1,1,'Finish project work','06/12/2021','10/12/2021'),(2,2,'Revise for AZ900 test','05/12/2021','15/12/2021');
 /*!40000 ALTER TABLE `User_Goals` ENABLE KEYS */;
 UNLOCK TABLES;
--- db/CreateUsersTable.sql
+++ db/CreateUsersTable.sql
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS Users
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (1,'Ben','test@test7.com','password'),(2,'Luke','test@test.com','password2')
+INSERT INTO `Users` VALUES (1,'Ben','test@test7.com','password'),(2,'Luke','test@test.com','password2');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
