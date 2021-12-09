b023cd50f0c7e0a171b98c9b2957a1fcf313bcb0
rachael-lh <rachaellouho@gmail.com>
2021-12-09 17:18:13 +0000

    added foreign key constraint userid = users.id

--- db/CreateUserGoals.sql
+++ db/CreateUserGoals.sql
@@ -1,15 +1,17 @@
 CREATE TABLE IF NOT EXISTS usergoals
              (
                           id              INTEGER NOT NULL AUTO_INCREMENT,
+                          userid          INTEGER, 
                           goals           VARCHAR(30) NOT NULL,
                           date_started    VARCHAR(50) NOT NULL,
                           date_endgoal    VARCHAR(150) NOT NULL,
-                          PRIMARY KEY (id)
+                          PRIMARY KEY (id),
+                          FOREIGN KEY (userid) REFERENCES users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 LOCK TABLES `usergoals` WRITE;
 /*!40000 ALTER TABLE `usergoals` DISABLE KEYS */;
-INSERT INTO `usergoals` VALUES (1,'Hello','12/15/2021','11/12/2021'),(2,'Bye','Monday','Friday');
+INSERT INTO `usergoals` VALUES (1,1,'Hello','12/15/2021','11/12/2021'),(2,2,'Bye','Monday','Friday');
 /*!40000 ALTER TABLE `usergoals` ENABLE KEYS */;
 UNLOCK TABLES;
--- flask-app/app.py
+++ flask-app/app.py
@@ -14,27 +14,39 @@ class Users(db.Model):
     full_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     user_password = db.Column(db.String(100), nullable=False)
+    BobaT = db.relationship('usergoals', backref='users', lazy=True)
     def __init__(self, full_name, email, user_password):
         self.full_name = full_name
         self.email = email
         self.user_password = user_password
 
-
+class usergoals(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    goals = db.Column(db.String(100), nullable=False)
+    date_started = db.Column(db.String(100), nullable=False)
+    date_endgoal = db.Column(db.String(100), nullable=False)
+    userid = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) 
+    def __init__(self, goals, date_started, date_endgoal, userid):
+        self.goals = goals
+        self.date_started = date_started
+        self.date_endgoal = date_endgoal
+        self.userid = userid
 
 @app.route('/')
 def index():
-    all_data = Users.query.all()
+    users_data = Users.query.all()
+    usergoals_data = usergoals.query.all()
 
-    return render_template("index.html", users = all_data)
+    return render_template("index.html", users = users_data, goals = usergoals_data)
 
 @app.route('/update', methods = ['GET', 'POST'])
 def update():
     if request.method == 'POST':
-        my_data = Users.query.get(request.form.get('id'))
+        my_data = usergoals.query.get(request.form.get('id'))
 
-        my_data.name = request.form['name']
-        my_data.email = request.form['email']
-      #  my_data.password = request.form['password']
+        my_data.goals = request.form['goals']
+        my_data.date_started = request.form['date_started']
+        my_data.date_endgoal = request.form['date_endgoal']
 
         db.session.commit()
         flash("User information updated successfully!")
@@ -46,12 +58,14 @@ def insert():
  
     if request.method == 'POST':
  
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
+
+        goals = request.form['goals']
+        date_started = request.form['date_started']
+        date_endgoal = request.form['date_endgoal']
+        userid = request.form['userid']
  
  
-        my_data = Users(name, email, phone)
+        my_data = usergoals(goals, date_started, date_endgoal, userid)
         db.session.add(my_data)
         db.session.commit()
  
@@ -62,7 +76,7 @@ def insert():
 
 @app.route('/delete/<id>/', methods = ['GET', 'POST'])
 def delete(id):
-    my_data = Users.query.get(id)
+    my_data = usergoals.query.get(id)
     db.session.delete(my_data)
     db.session.commit()
     flash("User Deleted Successfully")
